1cc17c5cd7fa49e606ac26a67e38b69d71558f95
Thomas Massmann <thomas@propertyshelf.com>
2015-02-27 23:29:28 +0100

    Initial commit.
    - Added diazo_theme template for core diazo packages (themes).

---
+++ setup.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""Setup for bobtemplates.propertyshelf package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+version = '0.1dev'
+description = 'Mr.Bob Templates for Propertyshelf projects.'
+long_description = ('\n'.join([
+    open('README.rst').read(),
+    open('CHANGES.rst').read(),
+]))
+
+install_requires = [
+    'setuptools',
+    # -*- Extra requirements: -*-
+    'mr.bob',
+]
+
+setup(
+    name='bobtemplates.propertyshelf',
+    version=version,
+    description=description,
+    long_description=long_description,
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Utilities",
+    ],
+    keywords='',
+    author='Propertyshelf, Inc.',
+    author_email='development@propertyshelf.com',
+    url='https://github.com/propertyshelf/bobtemplates.propertyshelf',
+    download_url='http://pypi.python.org/pypi/bobtemplates.propertyshelf',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['bobtemplates'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={
+        'test': [
+            'coverage',
+            'nose',
+            'nose-selecttests',
+            'scripttest',
+            'unittest2',
+        ]
+    },
+    entry_points={},
+)
